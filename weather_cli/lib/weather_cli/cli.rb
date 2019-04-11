dfe2ea33d50d1370aba952b963b3a9bfb5e5d55b
Mickae Truman <mtruman@nyit.edu>
2019-04-11 03:43:46 +0000

    inputcode

--- weather_cli/lib/weather_cli/cli.rb
+++ weather_cli/lib/weather_cli/cli.rb
@@ -5,6 +5,8 @@ class WeatherCli::CLI
   def call 
     puts "Today's forecast:"
     temperature
+    navigation
+    goodbye
   end
   
   def temperature
@@ -13,4 +15,23 @@ class WeatherCli::CLI
     2. New York : 47 degrees
   DOC
 end
+
+def navigation
+  puts "Enter the city's corresponding number to see forecast information details or type exit:"
+  input = nil 
+  while input != "exit"
+  puts "Enter the city's corresponding number to see forecast information details or type exit:"
+    input = gets.strip
+    case input 
+    when "1"
+      puts "Details on city 1..."
+    when "2"
+      puts "Details on city 2..."
+    end
+  end
+end
+
+def goodbye
+  puts "Have a fantastic day!"
+  end
 end 
