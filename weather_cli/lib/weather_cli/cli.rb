f8960142840f00cb3dfcb243752c13ec4398a466
Mickae Truman <mtruman@nyit.edu>
2019-04-11 03:28:47 +0000

    added heredoc test

--- weather_cli/lib/weather_cli/cli.rb
+++ weather_cli/lib/weather_cli/cli.rb
@@ -4,6 +4,13 @@ class WeatherCli::CLI
   
   def call 
     puts "Today's forecast:"
+    temperature
   end
   
+  def temperature
+    puts <<-DOC
+    1. Wheatley Heights : 44 degrees
+    2. New York : 47 degrees
+  DOC
+end
 end 
