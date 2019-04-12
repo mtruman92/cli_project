0fe181124e73083f8f4347701d1f54e8614d2257
Mickae Truman <mtruman@nyit.edu>
2019-04-12 15:41:34 +0000

    added usstates.rb & scraper

--- weather_cli/lib/weather_cli/scraper.rb
+++ weather_cli/lib/weather_cli/scraper.rb
@@ -1,13 +1,49 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
+class States::Scraper
 
-html = open("https://www.history.com/topics/us-states")
+def initalize(url = https://www.history.com/topics/us-states)
+@url = url 
+end
 
-Nokogiri::HTML(html)
+#scrapes a history website featuring US 50 states and returns an array of States 
+def us_states
+@doc = Nokogiri::HTML(open(@url))
+state_links =
+id = 0
 
-doc = Nokogiri::HTML(html)
+state_links.collect do |states|
+id += 1
+s = States::UnitedStates.new
+s.id = id
+s.description =
+s.date_of_statehood =
+s.capital =
+s.population =
+s.size =
+s.interesting_facts =
+
+state_links =
+state_name =
+description =
+date_of_statehood =
+capital =
+population =
+size =
+interesting_facts =
+
 #binding.pry
-#puts doc
-#binding.pry
-puts doc
+
+#Returns an updated title of the website
+def get_title
+title =
+end 
+
+#Returns an array with state info from its website
+def get_info(state_id)
+href =
+
+state_page = Nokogiri::HTML(open(href))
+
+#Scrapes an individual state website and printsa document
+end
+
+end
---
+++ weather_cli/lib/weather_cli/usstates.rb
@@ -0,0 +1,9 @@
+class States::UnitedStates
+
+attr_accessor :state, :date_entry, :capital :population :size, :href
+
+def initialize(state)
+@state = state 
+end
+
+end 
