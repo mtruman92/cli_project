20b049e939d29144b0ec0797f347cdb85bf300c8
Mickae Truman <mtruman@nyit.edu>
2019-04-12 03:51:25 +0000

    changed project topic

--- weather_cli/lib/weather_cli/cli.rb
+++ weather_cli/lib/weather_cli/cli.rb
@@ -3,30 +3,79 @@
 class WeatherCli::CLI 
   
   def call 
-    puts "Today's forecast:"
-    temperature
+    puts "Welcome to the US States Information Center:"
     navigation
+    welcome
     goodbye
   end
   
-  def temperature
+  def navigation
     puts <<-DOC
-    1. Wheatley Heights : 44 degrees
-    2. New York : 47 degrees
+       1 : Alabama
+       2 : Alaska
+       3 : Arizona
+       4 : Arkansas
+       5 : California
+       6 : Colorado
+       7 : Connecticut
+       8 : Delaware
+       9 : Florida
+      10 : Georgia
+      11 : Hawaii
+      12 : Idaho
+      13 : Illinois
+      14 : Indiana
+      15 : Iowa
+      16 : Kansas
+      17 : Kentucky
+      18 : Louisiana
+      19 : Maine
+      20 : Maryland
+      21 : Massachusetts
+      22 : Michigan
+      23 : Minnesota
+      24 : Mississippi
+      25 : Missouri
+      26 : Montana
+      27 : Nebraska
+      28 : Nevada
+      29 : New Hampshire
+      30 : New Jersey
+      31 : New Mexico
+      32 : New York
+      33 : North Carolina
+      34 : North Dakota
+      35 : Ohio
+      36 : Oklahoma
+      37 : Oregon
+      38 : Pennsylvania
+      39 : Rhode Island
+      40 : South Carolina
+      41 : South Dakota
+      42 : Tennessee
+      43 : Texas
+      44 : Utah
+      45 : Vermont
+      46 : Virginia
+      47 : Washington
+      48 : West Virginia
+      49 : Wisconsin
+      50 : Wyoming
   DOC
 end
 
-def navigation
-  puts "Enter the city's corresponding number to see forecast information details or type exit:"
+def welcome
+  puts "Choose a state to get started!"
+  puts "Enter the state's corresponding number to see state details or type exit:"
   input = nil 
   while input != "exit"
-  puts "Enter the city's corresponding number to see forecast information details or type exit:"
+  puts "Enter the state's corresponding number to see state details or type exit:"
     input = gets.strip
     case input 
     when "1"
-      puts "Details on city 1..."
+      puts "Details on state 1..."
     when "2"
-      puts "Details on city 2..."
+      puts "Details on state 2..."
     end
   end
 end
--- weather_cli/lib/weather_cli/scraper.rb
+++ weather_cli/lib/weather_cli/scraper.rb
@@ -1,10 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
 
 Nokogiri::HTML(html)
 
 doc = Nokogiri::HTML(html)
-
+#binding.pry
+#puts doc
+binding.pry
 puts doc
